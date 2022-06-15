37c0a4144b5ae3821f5143faaa454e17b3651898
MattieChu <MattieChu@users.noreply.github.com>
2022-06-15 14:36:03 +0300

    change webpack, eslint conf, add eslint extend

--- webpack.common.js
+++ webpack.common.js
@@ -4,6 +4,7 @@ const StylelintPlugin = require('stylelint-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
 const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const path = require('path');
 
 const srcPath = path.resolve(__dirname, './src/');
@@ -48,15 +49,20 @@ module.exports = function (env, argv) {
           exclude: /node_modules/
         },
         {
-          test: /\.css$/,
+          test: /\.css$|\.scss$/,
           exclude: /node_modules/,
-          use: ['style-loader', 'css-loader', 'postcss-loader']
-        },
-        {
+          use: [
+            isDevServer ? 'stle-loader' : MiniCssExtractPlugin.loader,
+            { loader: 'css-loader' },
+            { loader: 'sass-loader' },
+            { loader: 'postcss-loader' }
+          ]
+        }
+        /*         {
           test: /\.s[ac]ss$/i,
           exclude: /node_modules/,
           use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader']
-        }
+        } */
       ]
     },
     plugins: [
@@ -78,6 +84,11 @@ module.exports = function (env, argv) {
       new StylelintPlugin(),
       new ESLintPlugin({
         extensions: ['ts', 'tsx', 'js', 'jsx']
+      }),
+      new MiniCssExtractPlugin({
+        // it extracts css-code from js into splitted file
+        filename: isDevMode ? '[name].css' : '[name].[contenthash].css',
+        chunkFilename: isDevMode ? '[id].css' : '[id].[contenthash].css'
       })
     ]
   };
