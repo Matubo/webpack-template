3021fb9c50c504dd694c2f6a495c817d86e142ed
Matubo <matbei14@yandex.ru>
2022-06-19 13:16:55 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,17 @@ import './index.css';
 
 export default function App() {
   console.log('jopa');
-  console.log('jopa');
+ 
+  
+  
+console.log('jopa');
   console.log('jopa');
   return (
     <div className={style.Jopa}>
       <p className={style.Hellow}>Hellow</p>
       <p className="test">ппц...</p>
       <Apps></Apps>
+      <div id={'10'} className='10'>Jopa</div>
     </div>
   );
 }
