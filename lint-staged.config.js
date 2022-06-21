11e79006717b396bf0170608972a8ab8055d5f24
MattieChu <MattieChu@users.noreply.github.com>
2022-06-21 15:28:32 +0300

    test

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   '*.{css,scss,less}': ['stylelint --fix  --cache', 'prettier -w .'],
-  '*.{js,ts,json}': ['eslint --fix --cache .', 'prettier -w .']
+  '*.{js,jsx,ts,tsx,json}': ['eslint --fix --cache .', 'prettier -w .']
   //"*.{html}": ["prettier --write"],
   //"*.{md}": ["markdownlint --fix **/*.md"],
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './index.css';
 
 export default function App() {
+  console.log('Jopa');
   return <div className="React App"></div>;
 }
