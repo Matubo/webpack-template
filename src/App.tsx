7bb09f64805d37dc89c778f37bc0fed41ab796a3
MattieChu <MattieChu@users.noreply.github.com>
2022-06-21 15:24:35 +0300

    del garbage

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,5 @@
-import style from './App.module.scss';
-import Apps from './Apps';
 import './index.css';
 
 export default function App() {
-  console.log('jopa');
-  console.log('jopa');
-  console.log('jopa');
-  console.log('jopa');
-  console.log('jopa');
-  console.log('jopa');
-  console.log('jopa');
-  console.log('jopa');
-  console.log('jopa');
-  return (
-    <div className={style.Jopa}>
-      <p className={style.Hellow}>Hellow</p>
-      <p className="test">ппц...</p>
-      <Apps></Apps>
-      <div id={'10'} className="10">
-        Jopa
-      </div>
-    </div>
-  );
+  return <div className="React App"></div>;
 }
